bd09c8b0e9ad28774050b669bddc5bb6a4f7d94b
Rodrigo Silva <rodrigo.92.srb@gmail.com>
2024-11-26 15:59:22 -0300

    Adding conversion service: next Step create ApiResponse Model

---
+++ src/app/services/conversion.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConversionService {
+
+  private url:string = 'http://localhost:8080/api/converter/jsontocsv';
+
+  constructor(private http: HttpClient) { }
+
+  convertJsonToCsv(json: string): Observable<ApiResponse>{
+    const headers = new HttpHeaders().set('Content-Type', 'application/json');
+    return this.http.post<ApiResponse>(this.url, json, {headers});
+  }
+
+}
