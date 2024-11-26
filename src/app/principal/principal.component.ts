feee1dda7d609954e605f8504ca4583224334e7b
Rodrigo Silva <rodrigo.92.srb@gmail.com>
2024-11-26 14:37:35 -0300

    Import FormsModule e HttpClient

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-principal',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.css'
 })
