55ffb292cc6a8b272191c009d7751eb146d1bce3
Rodrigo Silva <rodrigo.92.srb@gmail.com>
2024-11-26 20:29:02 -0300

    Adding convertJsonToCsv method and adjusts in style

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ConversionService } from '../services/conversion.service';
 
 @Component({
   selector: 'app-principal',
@@ -8,5 +9,20 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './principal.component.css'
 })
 export class PrincipalComponent {
+
+  csv: string = '';
+  jsonInput: string = '';
+
+  constructor(private conversionService: ConversionService) { }
+
+  convertJsonToCsv(json: string) {
+    this.conversionService.convertJsonToCsv(json).subscribe(response => {
+      if(response.success) {
+        this.csv = response.data;
+      }else {
+        console.log(response.message);
+      }
+    });
+  }
 
 }
