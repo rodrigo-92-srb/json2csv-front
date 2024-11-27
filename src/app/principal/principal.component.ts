0f24db7be1a7e65e89c72d7051d43949b4b6821f
Rodrigo Silva <rodrigo.92.srb@gmail.com>
2024-11-27 14:52:53 -0300

    Adding validation and incremental adjusts on interface

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -12,17 +12,39 @@ export class PrincipalComponent {
 
   csv: string = '';
   jsonInput: string = '';
+  apiMessage: string = '';
+  showSuccess: boolean = false;
+  showDanger: boolean = false;
 
   constructor(private conversionService: ConversionService) { }
 
   convertJsonToCsv(json: string) {
     this.conversionService.convertJsonToCsv(json).subscribe(response => {
       if(response.success) {
+        this.showSuccess = true;
+        this.showDanger = false;
         this.csv = response.data;
+        this.apiMessage = "Conversão realizada com sucesso!";
+        console.log(response);
       }else {
+        this.showSuccess = true;
+        this.showDanger = true;
+        this.apiMessage = response.message;
         console.log(response.message);
       }
+    }, error => {
+      this.showSuccess = false;
+      this.showDanger = true;
+      this.apiMessage = this.getErrorMessage(error.error.message);
     });
   }
 
+  getErrorMessage(errorMessage: string): string {
+    if (errorMessage.includes('JsonParseException')) {
+      return 'Erro ao parsear o JSON. Verifique se o JSON está correto.';
+    } 
+    else {
+      return 'Erro ao converter o JSON para CSV. Tente novamente.';
+    }
+  }
 }
